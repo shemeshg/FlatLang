0e2f2cefcba2562b3d082ab18a5b4dae107210c7
shemeshg <gili.shemesh@gmail.com>
2025-08-27 20:24:57 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -4,14 +4,54 @@
 using json = nlohmann::json;
 
 int main(int, char**){
-    std::cout << "Hello, from FlatLang!\n";
 
-    json ex1 = json::parse(R"(
-    {
-        "pi": 3.141,
-        "happy": true
+    /*
+    assume:
+    # External hardware bindings
+    gpio_in: external int[4] hw_gpio_in    
+    gpio_out: external int[4] hw_gpio_out
+    tick_counter: external int clock
+
+    toggle_start_stop: alias gpio_in[0]
+    */
+
+    nlohmann::json externalParams = nlohmann::json::array({
+        {
+            {"name", "gpio_in"},
+            {"datatype", "std::span<int,4>"},
+        },
+        {
+            {"name", "gpio_out"},
+            {"datatype", "std::span<int,4>"},
+        },
+        {
+            {"name", "clockTick"},
+            {"datatype", "int&"},
+        }
+    });
+
+
+
+    std::string t = R"(
+    #include <span>
+
+    void myRealTimeLoop(
+    {% for var in params %}   
+     {{ var.datatype }} {{ var.name }}{% if not loop.is_last %},{% endif %}    
+    {% endfor %}
+    ) {
+        {% include "impl" %}
     }
-    )");
+    )";
+
+    inja::Environment env;
+    inja::Template implTemplate = env.parse("    // Implementation goes here");
+    env.include_template("impl", implTemplate);
+
+
+nlohmann::json data;
+data["params"] = externalParams;
+
     
-    std::cout << inja::render("Hello {{ pi }} \n!", ex1);
+    std::cout << env.render(t, data);
 }
