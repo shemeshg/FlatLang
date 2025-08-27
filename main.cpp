b038f9787a2a877f7de1ffd72c7f004b94694398
shemeshg <gili.shemesh@gmail.com>
2025-08-27 17:42:52 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <inja.hpp>
+using json = nlohmann::json;
 
 int main(int, char**){
     std::cout << "Hello, from FlatLang!\n";
+
+    json ex1 = json::parse(R"(
+    {
+        "pi": 3.141,
+        "happy": true
+    }
+    )");
+    
+    std::cout << inja::render("Hello {{ pi }} \n!", ex1);
 }
